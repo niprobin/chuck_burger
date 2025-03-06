a87c634860197b2249a2101706ad0ad199205989
niprobin <niprobin@gmail.com>
2025-03-06 21:17:20 +0100

    navbar scroll effect

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(window).scroll(function(){
-    if ($(this).scrollTop() > 25) {
+    if ($(this).scrollTop() > 80) {
        $('#navbar').addClass('scrolled');
-       
+
     } else {
        $('#navbar').removeClass('scrolled');
     }
