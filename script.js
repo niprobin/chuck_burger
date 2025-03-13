e82ad3100e1cc604ebe26ab95795fb0144923482
niprobin <niprobin@gmail.com>
2025-03-13 18:24:23 +0100

    new layout

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 $(window).scroll(function(){
-    if ($(this).scrollTop() > 80) {
+    if ($(this).scrollTop() > 300) {
        $('#navbar').addClass('scrolled');
+       $('.nav-link').css("border-radius", "0px").css("box-shadow", "transparent 0px 0px 0px 0px");
 
     } else {
        $('#navbar').removeClass('scrolled');
+       $('.nav-link').css("border-radius", "4px").css("box-shadow", "rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px");
     }
 });
