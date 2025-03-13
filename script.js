a90b2ef4696badfac76a35d2fab14e046727ae38
niprobin <niprobin@gmail.com>
2025-03-13 19:26:03 +0100

    new menu for mobile

--- script.js
+++ script.js
@@ -8,3 +8,10 @@ $(window).scroll(function(){
        $('.nav-link').css("border-radius", "4px").css("box-shadow", "rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px");
     }
 });
+
+$(document).ready(function() {
+   $(".menu-btn").click(function() {
+       $(".menu").toggleClass("open");
+       $(".menu-btn i").toggleClass("fa-bars fa-times");
+   });
+});
