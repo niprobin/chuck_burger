9b593fc316f73d9af94353299043f1ea3f6b727b
niprobin <niprobin@gmail.com>
2025-03-14 15:14:06 +0100

    mobile friendly now

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 $(window).scroll(function(){
     if ($(this).scrollTop() > 200) {
        $('#navbar').addClass('scrolled');
-       $('.nav-link').css("border-radius", "0px").css("box-shadow", "none");
+       $('#logo-home').css("opacity", "1");
 
     } else {
        $('#navbar').removeClass('scrolled');
-       $('.nav-link').css("border-radius", "4px").css("box-shadow", "rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px");
+       $('#logo-home').css("opacity", "0");
     }
 });
 
